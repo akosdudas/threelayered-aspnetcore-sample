c837cf338be319632329afdf70d2e8276dd4d713
Dudas Akos <dudas.akos@aut.bme.hu>
2020-08-28 11:44:16 +0200

    Upgrade to .NET Core 3.1

--- src/Webshop.Web/Startup.cs
+++ src/Webshop.Web/Startup.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Webshop.BL;
 using Webshop.DAL;
 using Webshop.DAL.EF;
@@ -19,7 +19,6 @@ namespace Webshop.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<WebshopDb>(options => options.UseSqlServer(Configuration.GetConnectionString("WebshopDb")));
@@ -29,11 +28,12 @@ namespace Webshop.Web
 
             services.AddTransient<VevoManager>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRouting();
+            services.AddRazorPages();
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,11 +46,11 @@ namespace Webshop.Web
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
